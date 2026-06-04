f8717b658de16271872ba78515bc358018fa8f94
Dabson10 <dabson753@gmail.com>
2026-06-03 21:38:16 -0600

    Se empezo con la plantilla de la pagina web

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import './style.css'
 
 //Solo meto logica aqui
+// const btnTema = document.getElementById('btn-tema');
+// btnTema.addEventListener('click', () => {
+//     document.documentElement.classList.toggle('dark');
+// })
