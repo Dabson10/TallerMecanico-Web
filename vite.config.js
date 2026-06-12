fa008a3d389100409cda755bfa49d844178c832e
Dabson10 <dabson753@gmail.com>
2026-06-11 22:50:51 -0600

    Cambios pequeños.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                login: "login.html",
+                cliente: "cliente.html",
+                tecnico: "tecnico.html",
+            },
+        },
+    },
 })
