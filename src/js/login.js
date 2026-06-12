8d0774fb11c540102cbc673c1d2df77b0ca64916
Dabson10 <dabson753@gmail.com>
2026-06-12 17:54:48 -0600

    Se termino el login y cambios visuales

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,53 @@
 import '../main.js'
 
+// Mostrar contraseña
+const svgBtns = document.querySelectorAll('#btn-svg');
+const inpPass = document.querySelectorAll('#pass-cre');
+
+//Recorre los botoenes y verifica en cual sera su dirección.
+svgBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let data = btn.dataset.svg;
+        //Filtramos si el svg es para crear o iniciar sesion
+        if (data.includes('crear')) {
+            //Ahora si cambiamos el svg y el input
+            cambioSimple(data, btn, "crear")
+        } else if (data.includes('iniciar')) {
+            cambioSimple(data, btn, "iniciar")
+        }
+    });
+});
+/**
+ * Esta funcion sirve para no repetir dos veces el codigo.
+ * @param {*} dataBtn : Valor de data del boton.
+ * @param {*} btn : Boton que fue presionado.
+ * @param {*} localizado : Valor que sera pasado para saber si es crear o inciar.
+ */
+function cambioSimple(dataBtn, btn, localizado) {
+    let svgData = "";
+    if (dataBtn.includes('abierto')) {
+        //Si es abierto entonces cerramos el ojo y cambiamos el tipo de input
+        btn.classList.add('hidden')
+        //Localizamos el svg y hacemos cambios
+        svgData = "cerrado-" + localizado;
+        const cambiosvg = document.querySelector(`[data-svg="${svgData}"]`);
+        cambiosvg.classList.toggle('hidden');
+        //Hacemos el cambio del input
+        const inputPass = document.querySelector(`[data-input="${localizado}"]`);
+        inputPass.type = "text";
+        inputPass.placeholder = "Contraseña";
+    } else {
+        btn.classList.add('hidden')
+        svgData = "abierto-" + localizado;
+        const cambiosvg = document.querySelector(`[data-svg="${svgData}"]`);
+        cambiosvg.classList.toggle('hidden')
+        const inputPass = document.querySelector(`[data-input="${localizado}"]`);
+        inputPass.type = "password";
+        inputPass.placeholder = "••••••••••";
+
+    }
+}
+
 
 const cambioBtn = document.querySelectorAll('#btncambio');
 const formularios = document.querySelectorAll('.formulario');
